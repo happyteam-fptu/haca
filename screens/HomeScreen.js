618378b69beb4f25d84834f896a7cf8ace817ce7
DucQuanNguyen <113411704+DucQuanNguyen@users.noreply.github.com>
2022-10-09 18:55:39 +0700

    them cho truy cap profile, icon (img only)

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,23 +6,68 @@ import {
   TouchableOpacity,
   Image,
   StatusBar,
+  SafeAreaView,
 } from "react-native";
 import signOut from "../utilities/signOut";
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor="#f93" barStyle={"default"} />
       <Image
-        className="p-5 m-10 w-[90%] h-[20%] "
+        className="w-[90%] h-[20%]"
         source={require("../assets/anhbiapage.jpg")}
       />
+      <View className="bg-white mt-2 mb-8 w-[90%] ">
+        <TouchableOpacity className="flex-row w-[90%] h-[15%]">
+          <Image
+            source={require("../assets/logo-app.png")}
+            className="aspect-square w-[25%] shadow-sm"
+          />
+          <Text className="mx-2 h-20 text-2xl">hello</Text>
+        </TouchableOpacity>
+      </View>
+      <View className="flex-row w-[90%] h-[15%] justify-center align-middle pt-3 bg-green-300">
+        <TouchableOpacity className="flex-1 w-[15%] h-[15%]">
+          <Image
+            className="w-14 h-14"
+            source={require("../assets/imgLib.jpg")}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity className="flex-1 w-[15%] h-[15%]">
+          <Image className="w-14 h-14" source={require("../assets/list.jpg")} />
+        </TouchableOpacity>
+        <TouchableOpacity className="flex-1 w-[15%] h-[15%]">
+          <Image
+            className="w-14 h-14 bg-white "
+            source={require("../assets/friend.jpg")}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity className="flex-1 w-[15%] h-[15%]">
+          <Image
+            className="w-14 h-14"
+            source={require("../assets/birthday.jpg")}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity className="flex-1 w-[15%] h-[15%]">
+          <Image
+            className="w-14 h-14"
+            source={require("../assets/music.jpg")}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity className="flex-1 w-[15%] h-[15%]">
+          <Image
+            className="w-14 h-14 bg-white "
+            source={require("../assets/calendar.jpg")}
+          />
+        </TouchableOpacity>
+      </View>
       <TouchableOpacity onPress={() => signOut()}>
         <View className="bg-red-500 px-5 py-2 rounded-full">
           <Text>Logout</Text>
         </View>
       </TouchableOpacity>
-    </View>
+    </SafeAreaView>
   );
 };
 
