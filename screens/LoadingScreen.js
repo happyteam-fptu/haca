cabdf212609fb86d3b9ddbcd39fdcd1d969f0f8a
Tunna Duong <tunnaduong@gmail.com>
2022-11-03 07:47:38 +0700

    [BUG] Fixed redirection to welcome after loggedin

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -6,24 +6,18 @@ import { CommonActions } from "@react-navigation/native";
 const LoadingScreen = ({ route, navigation }) => {
   React.useEffect(() => {
     setTimeout(() => {
-      route?.params?.toScreen
-        ? navigation.dispatch(
-            CommonActions.reset({
-              index: 1,
-              routes: [
-                {
-                  name: route.params.toScreen,
-                  params: route.params?.params ? route.params.params : null,
-                },
-              ],
-            })
-          )
-        : navigation.dispatch(
-            CommonActions.reset({
-              index: 1,
-              routes: [{ name: "Welcome" }],
-            })
-          );
+      route?.params?.toScreen &&
+        navigation.dispatch(
+          CommonActions.reset({
+            index: 1,
+            routes: [
+              {
+                name: route.params.toScreen,
+                params: route.params?.params ? route.params.params : null,
+              },
+            ],
+          })
+        );
     }, 1300);
   });
 
