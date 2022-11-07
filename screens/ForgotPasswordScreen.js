a49a17038c1a6178fce29c855261f0d6601f2aad
Tunna Duong <tunnaduong@gmail.com>
2022-11-07 18:18:13 +0700

    Added in construction message at Forgot Password Screen

--- screens/ForgotPasswordScreen.js
+++ screens/ForgotPasswordScreen.js
@@ -5,7 +5,11 @@ const ForgotPasswordScreen = () => {
   return (
     <View>
       <StatusBar backgroundColor={"#fff"} barStyle="dark-content" />
-      <Text>ForgotPasswordScreen</Text>
+      <Text>
+        Tính năng đang phát triển! Hãy cùng đón chờ ở phiên bản sau nhé...{"\n"}
+        Bạn chưa có tài khoản? Hãy tìm file spreadsheet thông tin đăng nhập cho
+        các thành viên lớp tại group chat của lớp SE1741 trên Messenger nhé!
+      </Text>
     </View>
   );
 };
